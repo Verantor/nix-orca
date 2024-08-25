ad6a28dfe180b792b7bfaa104b59473ddd81d94f
Verantor <verantor@protonmail.com>
2024-08-25 18:14:59 +0200

    builder

--- main.go
+++ main.go
@@ -9,6 +9,33 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func output(build string, push string) {
+	fmt.Println("==================")
+	fmt.Printf("  Build: %v --> %v  \n", build, push)
+	fmt.Println("==================")
+}
+
+func build() {
+
+	cmd := exec.Command("nixos-rebuild", "build", "--flake", ".#main")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Println("rebuild failed: ", err)
+	} else {
+		cmd := exec.Command("nvd", "diff", "/run/current-system", "result")
+		if err := cmd.Run(); err != nil {
+			fmt.Println("nvd failed ", err)
+		} else {
+
+			cmd := exec.Command("sudo", "./result/activate")
+			if err := cmd.Run(); err != nil {
+				fmt.Println("failed to activate system ", err)
+			}
+		}
+	}
+}
+
 func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
@@ -22,61 +49,19 @@ func main() {
 						Name:    "local",
 						Aliases: []string{"l"}, //add to other functions
 						Usage:   "build on local machine",
-						Subcommands: []*cli.Command{
-							{
-								Name:    "remote",
-								Aliases: []string{"r"}, //add to other functions
-								Usage:   "pushes build to remote machine",
-								Action: func(cCtx *cli.Context) error {
-									fmt.Println("b on l and p2 r ", cCtx.Args().First())
-									cmd := exec.Command("nixbr")
-									cmd.Stdout = os.Stdout
-									if err := cmd.Run(); err != nil {
-										fmt.Println("could not run command: ", err)
-									}
-									return nil
-								},
-							},
-							{
-								Name:    "local",
-								Usage:   "pushes build to local machine",
-								Aliases: []string{"l"}, //add to other functions
-								Action: func(cCtx *cli.Context) error {
-									fmt.Println("b on l and p2 l ", cCtx.Args().First())
-									cmd := exec.Command("nixrb")
-									cmd.Stdout = os.Stdout
-									if err := cmd.Run(); err != nil {
-										fmt.Println("could not run command: ", err)
-									}
-									return nil
-									return nil
-								},
-							},
+						Action: func(cCtx *cli.Context) error {
+							output("l", "l")
+							build()
+							return nil
 						},
 					},
 					{
 						Name:    "remote",
 						Usage:   "build on remote machine",
 						Aliases: []string{"r"}, //add to other functions
-						Subcommands: []*cli.Command{
-							{
-								Name:    "local",
-								Aliases: []string{"l"}, //add to other functions
-								Usage:   "pushes build to local machine",
-								Action: func(cCtx *cli.Context) error {
-									fmt.Println("b on r and p2 l", cCtx.Args().First())
-									return nil
-								},
-							},
-							{
-								Name:    "remote",
-								Aliases: []string{"r"}, //add to other functions
-								Usage:   "pushes build to remote machine",
-								Action: func(cCtx *cli.Context) error {
-									fmt.Println("b on r and p2 r idk if it exists", cCtx.Args().First())
-									return nil
-								},
-							},
+						Action: func(cCtx *cli.Context) error {
+							build()
+							return nil
 						},
 					},
 				},
